c3e21a10c990ce27260f8af5e23dcc1489c6a693
bhavya chawla <bhavya9107@gmail.com>
2017-07-24 00:24:06 +0530

    Add files via upload

---
+++ js/model.js
@@ -0,0 +1,58 @@
+var places = [
+    {
+        "name": "Rashtrapati Bhawan",
+        "venue_id": "4d57c17fafe4b60c84784861",
+        "location" : {
+            "lat": 28.61436,
+            "lng":77.1974323
+        }
+    },
+    {
+        "name": "Qutub Minar",
+        "venue_id": "4ba47c9bf964a520fe9f38e3",
+        "location" : {
+            "lat": 28.5244281,
+            "lng": 77.1832672
+        }
+    },
+    {
+        "name": "India Gate",
+        "venue_id": "4b5eeab3f964a520ca9d29e3",
+        "location" : {
+            "lat": 28.612912,
+            "lng": 77.227321
+        }
+    },
+    {
+        "name": "Humayun's Tomb",
+        "venue_id": "4bf7829cb182c9b6f2f0755a",
+        "location" : {
+            "lat": 28.5932853,
+            "lng": 77.2485266
+        }
+    },
+    {
+        "name": "Red Fort",
+        "venue_id": "4bf214b83506ef3b1d7abd22",
+        "location" : {
+            "lat": 28.6561592,
+            "lng": 77.2388316
+        }
+    },
+    {
+        "name": "Ugrasen ki Baoli",
+        "venue_id":"4e070cdad22d658532a39289",
+        "location" : {
+            "lat": 28.6261132,
+            "lng": 77.2228093
+        }
+    },
+    {
+        "name": "Lotus Temple",
+        "venue_id": "4ba48dc3f964a520e2a338e3",
+        "location" : {
+            "lat": 28.553492,
+            "lng": 77.2566377
+        }
+    }
+]
